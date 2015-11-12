81bd025f8727916af8dba33abc83bab06b8b71c5
Fubu <fubu@thegame.ru>
2015-11-12 09:57:41 +0100

    Data for you!

---
+++ test.R
@@ -0,0 +1,23 @@
+library(igraph)
+g <- read.graph("data/lesmis.gml", format="gml")
+plot(g)
+e <- E(g)
+
+
+
+apply(datos,2,func)
+
+res <- sapply(dato,functio)
+
+
+sapply(1:10, function(x) x^2)
+
+karate <- graph.famous("Zachary")
+fc <- fastgreedy.community(karate)
+dendPlot(fc)
+
+modularity(fc)
+
+1/(2m)*sum((Aij-ki*kj/(2m)) delta(ci,cj),i,j)
+
+1/4m*(m_c − E[m_c])
