7170f476c6df4a009df63d0386a61dc6a0ff7868
Jordi Coll Caballero <u1906230@campus.udg.edu>
2015-11-24 12:57:21 +0100

    primera tabla todo 0 0 0 0

--- script.R
+++ script.R
@@ -11,22 +11,32 @@ com_algs = c(
   walktrap.community,
   infomap.community)
 
-karate <- graph.famous("Zachary")
+com_algs_names= c(
+  "edge_betweenness",
+  "fastgreedy",
+  "label_propagation",
+  "leading_eigenvector",
+  "multilevel",
+  "optimal",
+  "spinglass",
+  "walktrap",
+  "infomap")
 
-com <- edge.betweenness.community(karate)
-length(com)
-com[1]
-
-length(V(karate))
-
-calculatefc <- function(graph, clusters, idCluster) {
-  v <- unlist(clusters[idCluster]) #List of members
-  n <- unlist(neighbors(graph,unlist(v))) #List of neigbours
-  ocn <- setdiff(n,v) #Extract the n-v (in the set sense)
-  return(length(ocn))
+calculate_mcfc <- function(graph, clusters, idCluster) {
+  m <- 0
+  f <- 0
+  vlist <- unlist(clusters[idCluster]) #List of members
+  for(v in vlist){
+    n <- unlist(neighbors(graph,unlist(v))) #List of neigbours
+    ocn <- length(setdiff(n,v))
+    f <- f + ocn #Extract the n-v (in the set sense)
+    m <- m + length(n) - ocn
+  }
+  return(data.frame(m=m/2,f=f))
 }
 
 getValues <- function(graph){
+  dfres <- NULL
   for(i in 1 : length(com_algs)){
     n <- length(V(graph))
     com_alg <- com_algs[[i]]
@@ -36,28 +46,40 @@ getValues <- function(graph){
     avg_conductance <- 0
     avg_cut_ratio <- 0
     avg_expansion <- 0
-    
     for(c in 1:nComs){
-      mc <- length(com[c])
-      fc <- calculate_fc(graph, com, c) #nr. edges in the frontier of C
+      nc <- length(com[c])
+      mcfc <- calculate_mcfc(graph, com, c) #nr. edges in the frontier of C
+      fc <- mcfc$f
+      mc <- mcfc$m
       conductance <- fc / (2*mc + fc)
       expansion <- fc/nc
       cut_ratio <- fc/(nc*(n-nc))
-      avg_conductance <- avg_conductance + conductance*(mc/n)
-      avg_expansino <- avg_expansion + expansion*(mc/n)
-      avg_cut_ratio <- avg_cut_ratio + cut_ratio*(mc/n)
+      avg_conductance <- avg_conductance + conductance*(mc)
+      avg_expansino <- avg_expansion + expansion*(mc)
+      avg_cut_ratio <- avg_cut_ratio + cut_ratio*(mc)
     }
-    modularity(wc)
-    
-    membership(wc) #dice cada nodo a que cluster pertenede
+    df <- data.frame(rowname=com_algs_names[i],
+                     modularity=modularity(com),
+               conductance=avg_conductance/nComs,
+               expansion=avg_expansion/nComs,
+               cut_ratio=avg_cut_ratio/nComs)
+    dfres <- rbind(dfres, df)
     
     
   }
-  plot(wc, graph)
-  plot(graph, vertex.color=membership(wc))
+  #plot(wc, graph)
+  #plot(graph, vertex.color=membership(wc))
   #dendPlot(fc)
-  return(0)
+  return(dfres)
 }
 
 
-getValues(karate)
+karate <- graph.famous("Zachary")
+
+#com <- edge.betweenness.community(karate)
+#length(com)
+#com[1]
+
+#length(V(karate))
+
+values<-getValues(karate)
