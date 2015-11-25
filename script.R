cdf1245a99565f9909f69a868d165530d5d96e54
Fubu <fubu@thegame.ru>
2015-11-26 00:09:29 +0100

    last update

--- script.R
+++ script.R
@@ -76,7 +76,7 @@ getValues <- function(graph){
 }
 
 
-karate <- graph.famous("Zachary")
+
 
 #com <- edge.betweenness.community(karate)
 #length(com)
@@ -95,9 +95,14 @@ save(gValues, file="data/out/dummyValues.dat")
 
 
 #Zachary
+karate <- graph.famous("Zachary")
 karateValues<-getValues(karate)
 xtable(karateValues[[1]], digits=5)
 save(karateValues, file="data/out/karateValues.dat")
+
+karateCom <- com_algs[[2]](as.undirected(karate))
+plot(karateCom,karate)
+
 
 #Les miserables
 lesmis <- read.graph("data/lesmis.gml", format="gml")
@@ -105,11 +110,19 @@ lesmisValues <- getValues(lesmis)
 xtable(lesmisValues[[1]], digits=5)
 save(lesmisValues, file="data/out/lesmisResult.dat")
 
+lesmisCom <- com_algs[[2]](as.undirected(lesmis))
+lesLay <- layout.davidson.harel(lesmis)
+plot(lesmisCom,lesmis,layout=lesLay)
+
+layout.star
+
 #Dolphins
 dolphin <- read.graph("data/dolphins.gml", format="gml")
 dolphinValues <- getValues(dolphin)
 xtable(dolphinValues[[1]], digits=5)
 save(dolphinValues, file="data/out/dolphinValues.dat")
+dolphinCom <- com_algs[[2]](as.undirected(dolphin))
+plot(dolphinCom,dolphin)
 
 #Wiki
 
@@ -137,4 +150,18 @@ getLabels <- function(nodes,graph) {
   V(graph)[unlist(nodes)]$label
 }
 
-getLabels(wikiCom[100],wiki) #volcanoes
+length(wikiCom)
+getLabels(wikiCom[100],wiki) #volcanoescom
+getLabels(wikiCom[133],wiki)
+getLabels(wikiCom[39],wiki) #Chemistry
+getLabels(wikiCom[200],wiki) #Monacho chungos
+getLabels(wikiCom[72],wiki) #Nightwork and maternity
+getLabels(wikiCom[75],wiki) #Civil unions 
+getLabels(wikiCom[76],wiki) #Microscopes
+getLabels(wikiCom[169],wiki) #Dynasties
+
+#Random community exploration
+(r <-runif(1,min=1,max=length(wikiCom))); getLabels(wikiCom[r],wiki)
+
+which.max(sapply(wikiCom, length)) #Get bigger community
+getLabels(wikiCom[3],wiki) #Mathematic related stuff (bigger community)
